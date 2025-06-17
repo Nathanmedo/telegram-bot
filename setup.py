e89d3a70296357365fe03566de2dc850b93dc945
Nathanmedo <ihemedochinedu@gmail.com>
2025-06-17 18:11:30 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="your_project_name",        # Replace with your actual project name
+    name="telegram_bot",        # Replace with your actual project name
     version="0.1.0",                 # Or pull from your __init__.py
-    author="Your Name",
-    author_email="you@example.com",
-    description="A short description of your project",
+    author="Nathanmedo_devs",
+    author_email="ihemedochinedu@gmail.com",
+    description="A telegram bot for cryptocurrency trading and mining",
     packages=find_packages(),        # Automatically finds your Python packages
     install_requires=[],             # Add any dependencies here, e.g. ["requests"]
     python_requires=">=3.8",         # Set your minimum Python version
